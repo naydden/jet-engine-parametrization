51facc606d55550eff1f4d558a05753c53e0ae34
Boyan Naydenov <boyan.naydenov@gmail.com>
2018-05-21 11:55:49 +0200

    Minor improvements

--- code/functions/Fadimensional.m
+++ code/functions/Fadimensional.m
@@ -2,7 +2,8 @@ function [ Fadim] = Fadimensional( f,M9,M19,alpha,T,P,gam,mixer,T0,M0,P0)
 if mixer == true
     alpha=0;
     gam.hot=gam.mixer;
- 
+    T.s19 = T.s9;
+    P.s19 = P.s9;
 end
 
 Fadim=(1+f)*M9*sqrt(T.s9/T0)-M0+alpha*(M19*sqrt(T.s19/T0)-M0)+(1+f)/(M9*gam.hot)*sqrt(T.s9/T0)*(1-P0/P.s9)+alpha/(M19*gam.cold)*sqrt(T.s19/T0)*(1-P0/P.s19);
