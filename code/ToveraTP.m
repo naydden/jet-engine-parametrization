7be5fd2ed9a09c8402031bd3a49ef6c3938e592c
AstroProjects <pol.f.48@gmail.com>
2018-05-23 04:23:56 +0200

    Warning

--- code/ToveraTP.m
+++ code/ToveraTP.m
@@ -9,7 +9,7 @@ P9 = P0;
 M9 = sqrt(2/(gam.hot-1)*((P.t9/P9)^((gam.hot-1)/gam.hot)-1));
 %Alert through workspace.
 if ~isreal(M9)
-    warning('M9 imaginary');
+    warning('M9 imaginary; Pt9/P9 = %.2d ', P.t9/P0);
 end
 end
 
