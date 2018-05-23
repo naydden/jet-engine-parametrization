f574b48b4f4836fbb5b72fd30938c2ed5cdd28e7
EvaMariaUrbano <eva1616@gmail.com>
2018-05-23 11:45:48 +0200

    Changes turboprop

--- code/functions/Compressor.m
+++ code/functions/Compressor.m
@@ -1,8 +1,11 @@
-function [ PI,P,TAU,T ] = Compressor( PI,P,gam,T,TAU,ETA )
+function [ PI,P,TAU,T ] = Compressor( PI,P,gam,T,TAU,ETA,isTurboProp )
 %IN: PI.c, PI.f, PI.cH, gam, T.t25, ETA.cH
 %OUT:TAU.cH, T.t3, P.t3, TAU.c, PI.cH
-
 PI.cH=PI.c/PI.f;
+if isTurboProp %No hi ha fan així que els valors 25 son iguals als valors 2
+    P.t25=P.t2;
+    T.t25=T.t2;
+end
 TAU.c=pi2tau(PI.c,gam.cold);
 P.t3=P.t25*PI.cH;
 TAUi.cH=pi2tau(PI.cH,gam.cold);
--- code/functions/TurbinaBaixa.m
+++ code/functions/TurbinaBaixa.m
@@ -1,6 +1,11 @@
-function [ T,TAU,PI,P ] = TurbinaBaixa(T,alpha,CP,f,ETA,gam,P,PI,TAU )
+function [ T,TAU,PI,P ] = TurbinaBaixa(T,alpha,CP,f,ETA,gam,P,PI,TAU,isTurboProp )
+if isTurboProp
+ TAU.tL = 1/(TAU.cH*TAU.r)*(TAU.r*TAU.cH*(TAU.r-1)+TAU.lamb)/(TAU.lamb-TAU.r*TAU.c+TAU.r);
+ T.t5=TAU.tL*T.t45;
+else
 T.t5=T.t45-(T.t25-T.t2)*(((1+alpha)*CP.cold)/((1+f)*CP.hot*ETA.mec));
 TAU.tL=T.t5/T.t45;
+end
 Ti.t5=T.t45-(T.t45-T.t5)/ETA.tL;
 TAUi.tL=Ti.t5/T.t45;
 PI.tL=tau2pi(TAUi.tL,gam.hot);
