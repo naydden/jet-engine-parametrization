8e52ce7d449ac464af83a1d3d4ca1c6121ca63c2
EvaMariaUrbano <eva1616@gmail.com>
2018-05-23 18:30:05 +0200

    Verificacio mixer

--- code/Verificacio ( pa no molestar)/test_mixer.m
+++ code/Verificacio ( pa no molestar)/test_mixer.m
@@ -26,4 +26,4 @@ gamma13 = 1.4;
 Cp13 = R13*gamma13/(gamma13-1);
 Cp5 = R5*gamma5/(gamma5-1);
 
-[M6, Tt6, Pt6] = mixer(Tt5, Pt5, Cp5, gamma5, Tt13, M13, Cp13, gamma13, alpha_p,M5)
+[M6, Tt6, Pt6] = mixertest(Tt5, Pt5, Cp5, gamma5, Tt13, M13, Cp13, gamma13, alpha_p,M5)
