1028b5a401b6aef097654c16b86d290ca92eec20
EvaMariaUrbano <eva1616@gmail.com>
2018-05-23 10:28:43 +0200

    Areas

--- code/functions/Area.m
+++ code/functions/Area.m
@@ -1,6 +1,6 @@
 function [ A ] = Area(M,gam,Pt,Tt,R,m)
 %Cálcul área a 5. M5=1
-MPF=sqrt(gam)*M*(1+((gam-1)/2)*M^2)^((-gam-1)/2*(gam-1));
+MPF=sqrt(gam)*M*(1+((gam-1)/2)*M^2)^(-(gam+1)/(2*(gam-1)));
 A=m/MPF*sqrt(Tt*R)/Pt;
 end
 
