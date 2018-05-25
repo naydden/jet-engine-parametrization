c31f158c4d7a33767fb1d6db0eb472d82e5b1414
AstroProjects <pol.f.48@gmail.com>
2018-05-25 15:08:50 +0200

    Pol iteration works

--- code/MAIN_TURBOFAN.m
+++ code/MAIN_TURBOFAN.m
@@ -85,9 +85,19 @@ elseif isTurboProp %Tovera si tenim turboprop i no tenim mixer
                  0.24   0.14    1.3
                  0.23   0.13    1.4
                  0.22   0.12    1.5
+                 0.21   0.11    1.55
+                 0.19   0.1     1.6
+                 0.175  0.09    1.65
+                 0.165  0.08    1.7
+                 0.145  0.07    1.8
+                 0.135  0.06    1.85
+                 0.12   0.05    1.9
+                 0.11   0.04    1.95
+                 0.1    0.03    2
+                 0.06   0.02    2.1
+                 0.05   0.01    2.15
                  ];
-                 
-        
+        [itI, itJ] = size(itVal);
         %thrust
     [ m0,mf,msec ] = Fluxosmasics( f,Fadim_TP,F,a0,0);
     Tcore = C.cin*CP.hot*T0/v0*m0; %Tcore
@@ -101,28 +111,40 @@ elseif isTurboProp %Tovera si tenim turboprop i no tenim mixer
     incD = 0.1; 
     finn = 170;
     inc_n = 5;
-    D = 1:incD:finD; %Diametres
+    D = 0.1:incD:finD; %Diametres
     n = 1:inc_n:finn; %velocitats
     %valors objectiu del gràfic
-    CTref = 0.09; CPref = 0.18; Jref = 1.7;
-    TOL = 1; 
+    TOL = 0.09; 
     finish = false;
     for diam = D
         for rev = n
             CTit = Tprop/(rho0*rev^2*diam^4);
             CPit = Pow/(rho0*rev^3*diam^5);
             Jit = v0/(rev*diam);
-            
-            %check convergence
-            if (abs(CTit - CTref)<TOL || abs(CPit - CPref)<TOL || abs(Jit - Jref)<TOL)
-                D = diam;
-                n = rev;
-                finish = true;
-                break;
+            for i = 1:itI
+                CTref = itVal(i, 2);
+                CPref = itVal(i, 1);
+                Jref = itVal(i, 3);
+                %check convergence
+                if (abs(CTit - CTref)<TOL && abs(CPit - CPref)<TOL && abs(Jit - Jref)<TOL)
+                    D = diam;
+                    n = rev;
+                    CThrust = CTit;
+                    CPower = CPit;
+                    Jprop = Jit;
+                    finish = true;
+                    break;
+                end
+                if finish
+                    break;
+                end
             end
-            if finish
-                break;
+            if finish 
+                break
             end
+        end
+        if finish
+            break;
         end
     end
     
